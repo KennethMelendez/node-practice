93cc741f23a9356616d85353483c6bb82b72cc3a
KennethMelendez <kmlndz221@gmail.com.com>
2021-05-12 18:21:13 -0400

    feature: apply new url paths for local host

--- nodejsBasics/app.js
+++ nodejsBasics/app.js
@@ -1,10 +1,34 @@
 const http = require('http');
+const { title } = require('process');
 
 const port = 5050;
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
 
-server.listen(port, ()=>
-{
-    console.log(`server is not listening on port ${port}`);
+    switch (req.url) {
+        case '/':
+            res.end(`
+        <html>
+            <Head>
+                <title>
+                    first node app
+                </title>
+            </Head>
+            <body>
+                <h1>
+                    Welcome to my node application
+                </h1>
+            </body>
+        </html>
+        `);
+        break;
+        case '/admin':
+            res.end('Welcome to the admin page');
+        break;
+    };
+});
+
+
+server.listen(port, () => {
+    console.log(`server is now listening on port ${port}`);
 });
